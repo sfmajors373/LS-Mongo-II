5c7aaf84e851e298a065909093c299d1b59d8096
sfmajors373 <sfmajors373@gmail.com>
2017-08-17 20:30:13 -0400

    PostSchema and Populate configured

--- src/populate.js
+++ src/populate.js
@@ -1,6 +1,15 @@
 const fs = require('fs');
+const Posts = require('./post');
+const mongoose = require('mongoose');
+const posts = require('../posts.json');
 
 let savedPosts = null;
+
+// mongoose.Promise = global.Promise;
+// mongoose.connect(
+//   'mongodb://localhost/posts',
+//   { useMongoClient: true }
+// );
 
 const readPosts = () => {
   // cache posts after reading them once
@@ -11,8 +20,24 @@ const readPosts = () => {
   return savedPosts;
 };
 
+// const populate = () => {
 const populatePosts = () => {
   // TODO: implement this
+  readPosts();
+  const promises = savedPosts.map(p => new Posts(p).save());
+  return Promise.all(promises);
 };
-
+  // return populate()
+  //   .then(() => {
+  //     console.log('done');
+  //     mongoose.disconnect();
+  //   })
+  //   .catch((err) => {
+  //     console.log('ERROR', err);
+  //     throw new Error(err);
+  //   });
+// };
+populatePosts().then((allPosts) => {
+  return allPosts;
+});
 module.exports = { readPosts, populatePosts };
--- src/server.js
+++ src/server.js
@@ -1,12 +1,30 @@
 const bodyParser = require('body-parser');
 const express = require('express');
+const Posts = require('./post');
 
 const STATUS_USER_ERROR = 422;
+const STATUS_SERVER_ERROR = 500;
 
 const server = express();
 // to enable parsing of json bodies for post requests
 server.use(bodyParser.json());
 
 // TODO: write your route handlers here
+
+//GET /accepted-answer/:soID
+server.get('/accepted-answer/:soID', (req, res) => {
+  const { soID } = req.params;
+  Posts.findOne({})
+    .where('soID')
+    .equals(soID)
+    .exec((err, posts) => {
+      if (err) {
+        res.status(STATUS_SERVER_ERROR);
+	res.json({ error: err });
+      } else {
+
+      }
+    });
+});
 
 module.exports = { server };
